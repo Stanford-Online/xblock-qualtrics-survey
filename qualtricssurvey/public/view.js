f99e567fa1ad452a5a5d5866bacb4493e0817633
stv <stv@stanford.edu>
2019-03-06 19:40:30 -0800

    Fix ESLint issues

--- qualtricssurvey/public/view.js
+++ qualtricssurvey/public/view.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Initialize the QualtricsSurvey student view
+ * @param {Object} runtime - The XBlock JS Runtime
+ * @param {Object} element - The containing DOM element for this instance of the XBlock
+ * @returns {undefined} nothing
+ */
 function QualtricsSurveyView(runtime, element) {
     'use strict';
 
     var $ = window.jQuery;
     var $element = $(element);
+    /* eslint-enable no-unused-vars */
 
     // TODO: Put your logic here
 
