5c4e98c450a17da70738f8165b8f93cc42d35f7c
Aarif <mraarif@outlook.com>
2020-04-24 20:41:04 +0500

    updated the tox and travis configs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 
 
-version = '1.0.0'
+version = '1.1.0'
 description = __doc__.strip().split('\n')[0]
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst')) as file_in:
@@ -54,7 +54,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: JavaScript',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
         'Framework :: Django',
+        'Framework :: Django :: 2.2',
         'Topic :: Education',
         'Topic :: Internet :: WWW/HTTP',
     ],
